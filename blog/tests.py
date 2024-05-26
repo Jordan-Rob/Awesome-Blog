bde23a700abf44f25866e5f89c34b32dafc0d08f
Jordan Rob <jordanrob709@gmail.com>
2024-05-26 21:40:08 +0300

    Added Post status and Author relation

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
@@ -7,9 +8,15 @@ from .models import Post
 class PostTestCase(TestCase):
 
     def setUp(self):
-        Post.objects.create(title="testing post", slug="testing", body="a post to test")
+        user = User.objects.create(username="tester", password="@#seven")
         Post.objects.create(
-            title="testing post 2", slug="testing2", body="a post to test 2"
+            title="testing post", slug="testing", body="a post to test", author=user
+        )
+        Post.objects.create(
+            title="testing post 2",
+            slug="testing2",
+            body="a post to test 2",
+            author=user,
         )
 
     def test_post_properties(self):
@@ -22,3 +29,13 @@ class PostTestCase(TestCase):
         posts = Post.objects.all()
         self.assertEqual(posts[0].title, "testing post 2")
         self.assertEqual(posts[1].title, "testing post")
+
+    def test_author_blog_posts_relation(self):
+        user = User.objects.get(username="tester")
+        posts = user.blog_posts.all()
+        self.assertEqual(posts[0].title, "testing post 2")
+        self.assertEqual(posts[1].title, "testing post")
+
+    def test_default_blog_post_status(self):
+        post = Post.objects.get(title="testing post")
+        self.assertEqual(post.status, "DF")
